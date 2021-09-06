a2a5519dedb95f0bae9bff9baf026963f69ff057
gitthisjoel <joelbacke10@gmail.com>
2021-09-06 21:28:20 +0200

    first working version

--- main/generate_tex.py
+++ main/generate_tex.py
@@ -1,24 +1,40 @@
 import json
 from datetime import datetime
 
-master={
-  "cafe":
-    {"name": "Love Barany", "stilid": "lo5050ba-s"},
-  "sex":
-    {"name": "Marie Ask Uggla", "stilid": "ma4525as-s"},
-  "aktu":
-    {"name": "Sean Jentz", "stilid": "se5362je-s"}
+master = {
+    "cafe": {
+        "proper_name": "Cafémästeriet",
+        "name": "Love Barany",
+        "stilid": "lo5050ba-s",
+    },
+    "sex": {
+        "proper_name": "Sexmästeriet",
+        "name": "Marie Ask Uggla",
+        "stilid": "ma4525as-s",
+    },
+    "aktu": {
+        "proper_name": "Aktivitetsutskottet",
+        "name": "Sean Jentz",
+        "stilid": "se5362je-s",
+    },
 }
 
-def gen_tex(utskott, date, sales):
+with open("account_details.json", "r") as f:
+    accout_details = json.load(f)
+    f.close()
+
+
+def gen_tex(utskott, date, sales, pay_type):
     global master
+    global accout_details
 
-    name=master[utskott]['name']
-    stilid=master[utskott]['stilid']
+    name = master[utskott]["name"]
+    stilid = master[utskott]["stilid"]
+    proper_name = master[utskott]["proper_name"]
 
-    today=datetime.now().strftime('%Y-%m-%d')
+    today = datetime.now().strftime("%Y-%m-%d")
 
-    packages="""\documentclass{article}
+    packages = r"""\documentclass{article}
     \usepackage[utf8]{inputenc}
     \usepackage[a4paper, margin=0.8in]{geometry}
     \usepackage{palatino}
@@ -26,59 +42,73 @@ def gen_tex(utskott, date, sales):
     \usepackage{longtable}
     """
 
-    total=0
-    varor = """"""
-    for product in sales:
-        quantity = product["quantity"]
-        price_per_product = float('.'.join(product["price"].split(',')))
-        account = product["account"]
-        total += quantity*price_per_product
+    total = 0
+    varor = r"\newcommand{\varor}{"
+    account_report = {}
+    print(sales)
+    for product, info in sales.items():
+        print(product)
+        quantity = info["quantity"]
+        price_per_product = info["price"]  # float(".".join(info["price"].split(",")))
+        account = info["account"]
+        total += quantity * price_per_product
 
-        varor.append()
+        if account in account_report:
+            account_report[account] += total
+        else:
+            account_report[account] = total
 
-    commands_1 = """\newcommand{\skapad}{%s}
+        varor += (
+            f"{product} & {account} & {quantity} & {price_per_product} & {total}"
+            + r"\\"
+        )
+
+    varor += "}"
+    commands_1 = r"""\newcommand{\skapad}{%s}
     \newcommand{\kostnadsstalle}{%s}
     \newcommand{\typ}{%s}
     \newcommand{\datum}{%s}
     \newcommand{\summa}{%s kr}
     \newcommand{\namn}{%s}
     \newcommand{\stilid}{%s}
-    """ % (today, utskott, "Swish", date, total, name, stilid)
+    """ % (
+        today,
+        proper_name,
+        pay_type,
+        date,
+        total,
+        name,
+        stilid,
+    )
 
-    description = """\newcommand{\beskrivning}{Försäljning av varor i Cafe}"""
+    if utskott == "cafe":
+        description = r"""\newcommand{\beskrivning}{Försäljning av varor i Café}"""
+    elif utskott == "sex":
+        description = r"""\newcommand{\beskrivning}{Sexmästeri verksamhet}"""
+    elif utskott == "aktu":
+        description = (
+            r"""\newcommand{\beskrivning}{Försäljning av varor i Aktivitetsutskottet}"""
+        )
+    else:
+        description = r"""\newcommand{\beskrivning}{Försäljning}"""
 
-    commands_2 = """
-    \newcommand{\fordelning}{3000 & Försäljning & 4500 \\}
-    \newcommand{\varor}{Bil & 3000 & 1 & 4500 & 4500 \\}
-    """
+    fordelning = r"\newcommand{\fordelning}{"
+    for acc, s in account_report.items():
+        fordelning += f"{acc} & {accout_details[str(acc)]} & {s}" + r"\\"
+    fordelning += "}"
 
-    rest = ""
-    return packages+commands_1+description+commands_2+rest
-
-if __name__ == "__main__":
-    filename="values.json"
-    with open(filename) as file:
-        data=json.load(file)
-        file.close()
-
-    for utskott, values in data.items():
-        for date, sales in values.items():
-            tex = gen_tex(utskott, date, sales)
-
-
-
-    rest = """\begin{document}
+    rest = r"""\begin{document}
     \hspace{-0.3in}
     \begin{tabular}{p{1.0in}p{3.5in}p{2in}}
-        \includegraphics[width=0.8in]{D-symbol.pdf} &
-        \vspace{-1in}
-        {\large \uppercase{D-sektionen inom TLTH}} \newline \newline
-        {\Huge \textsf{\textbf{Intäktsräkning}}}
-        \newline \newline
-        {\large \typ}&
-        \vspace{-1in}
-        Skapad: \newline
-        \skapad \newline
+    \includegraphics[width=0.8in]{D-symbol.pdf} &
+    \vspace{-1in}
+    {\large \uppercase{D-sektionen inom TLTH}} \newline \newline
+    {\Huge \textsf{\textbf{Intäktsräkning}}}
+    \newline \newline
+    {\large \typ}&
+    \vspace{-1in}
+    Skapad: \newline
+    \skapad \newline
     \end{tabular}
 
     \vspace{0.5in}
@@ -130,3 +160,24 @@ if __name__ == "__main__":
     \end{tabular}
     \end{document}
     """
+    return packages + commands_1 + description + fordelning + varor + rest
+
+
+if __name__ == "__main__":
+    filename = "values.json"
+    with open(filename) as file:
+        sale_report = json.load(file)
+        file.close()
+
+    today = datetime.now().strftime("%Y-%m-%d")
+    nbr = 1
+    for pay_type, data in sale_report.items():
+        for utskott, values in data.items():
+            for date, sales in values.items():
+                tex = gen_tex(utskott, date, sales, pay_type)
+
+                filename = "tex/" + today + "_" + str(nbr) + ".tex"
+                with open(filename, "w", encoding="utf-8") as f:
+                    f.write(tex)
+                    f.close
+                nbr += 1
