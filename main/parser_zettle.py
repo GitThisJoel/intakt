68acb3df1c249b40741327a34d8218cec20fb8c2
gitthisjoel <joelbacke10@gmail.com>
2021-09-12 23:57:34 +0200

    fixed so making correct file

--- main/parser_zettle.py
+++ main/parser_zettle.py
@@ -1,14 +1,15 @@
 import json
+from sys import prefix
 from account import account_of
 
-infile = "Zettle-Sales-By-Product-Report-20210522.csv"
+infile = "../Zettle-Sales-By-Product-Report-20210823.csv"
 outfile = "zettle_sales.json"
-date = "2021"
-utskott_dict = {"s": "sex", "c": "cafe", "d": "aktu"}
+date = "2021-10-23"
+utskott_dict = {"s": "sex", "c": "cafe", "d": "aktu", "n": "noll"}
 
 if __name__ == "__main__":
     line_skips = 6
-    with open(infile, encoding="utf-8") as f:
+    with open(infile, encoding="unicode_escape") as f:
         i = 0
         lines = []
         for line in f:
@@ -23,8 +24,14 @@ if __name__ == "__main__":
 
         zettle_values = {}
         for line in lines[:-1]:
-            prefix, product = line[inds[0]].split("-")
-            utskott = utskott_dict[prefix[:-1]]
+            prefix_product = line[inds[0]].split("-")
+            prefix = prefix_product[0]
+            product = "-".join(prefix_product[1:])
+
+            if len(prefix) == 1:
+                utskott = utskott_dict[prefix]
+            else:
+                utskott = utskott_dict[prefix[:-1]]
             account = account_of(prefix)
 
             nbr_sold = float(".".join(line[inds[1]].split(",")))
@@ -41,14 +48,18 @@ if __name__ == "__main__":
 
             if utskott in zettle_values:
                 if date in zettle_values[utskott]:
-                    zettle_values[utskott][date].append(summary)
+                    zettle_values[utskott][date][product.capitalize()] = {
+                        "quantity": nbr_sold,
+                        "price": price_per_item,
+                        "account": account,
+                    }
                 else:
-                    zettle_values[utskott][date] = [summary]
+                    zettle_values[utskott][date] = summary
             else:
-                zettle_values[utskott] = {date: [summary]}
+                zettle_values[utskott] = {date: summary}
 
     with open(outfile, "w") as f:
-        out = json.dumps(zettle_values, indent=2, ensure_ascii=False)
+        out = json.dumps({"zettle": zettle_values}, indent=2, ensure_ascii=False)
         # print(out)
         f.write(out)
         f.close()
