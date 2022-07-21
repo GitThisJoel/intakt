fae63b72f2bc12e9f50569c15347492ce0c13f18
GitThisJoel <joelbacker10@gmail.com>
2022-07-21 12:14:22 +0200

    moved args to here, this will be used if user want to use cmd-line

---
+++ main/main.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-s",
+        "--source",
+        default="zettle",
+        help="Where the data is supposed to be retrieved from [z(ettle)|s(wish)]",
+    )
+
+    parser.add_argument(
+        "-sd",
+        "--start-date",
+        help="The start date for the fetch of Zettle sales. Date is on ISO format: yy-mm-ddThh:mm:ss, time is default to zero",
+    )
+
+    parser.add_argument(
+        "-ed",
+        "--end-date",
+        default=None,
+        nargs="?",
+        help="The end date for the fetch of Zettle sales, same format as start_date",
+    )
+
+    parser.add_argument(
+        "-td",
+        "--time-delta",
+        default="daily",
+        nargs="?",
+        help="The time of which each report spans",
+    )
+
+    args = vars(parser.parse_args())
+
+    print(args)
+    source = args["source"]
+    start_date = datetime.fromisoformat(args["start_date"])
+    end_date = datetime.fromisoformat(args["end_date"]) if args["end_date"] is not None else None
+    time_delta = args["time_delta"]
+
+    sales = get_zettle_purchases.get_sales(start_date, end_date)
+    return parse(sales, time_delta)
+
+
+if __name__ == "__main__":
+    main()
