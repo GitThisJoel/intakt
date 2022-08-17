46bb3bfde62deb61831b8a7912c24d2eea668119
GitThisJoel <joelbacker10@gmail.com>
2022-08-17 15:37:40 +0200

    wrote the parse method for zettle

--- main/parsers/zettle_parser.py
+++ main/parsers/zettle_parser.py
@@ -1,6 +1,3 @@
-# Used to retreive Zettle purchase data using their API.
-
-
 # Used to generate the JSON-object from the Zettle API respnose.
 import sys, os
 
@@ -8,14 +5,58 @@ from datetime import datetime
 from datetime import timedelta
 from requests_oauthlib import OAuth2Session
 import json
+import dateutil.parser
+from flatten_dict import unflatten
 
 from parsers.parser import Parser
+from helpers.asset_loader import AssetLoader
+
+al = AssetLoader()
 
 
 class ZettleParser(Parser):
     @staticmethod
     def parse(data, time_delta):
-        pass
+        sales = {}
+        print(data)
+        for purchase in data["purchases"]:
+            amount = purchase["amount"]  # in öre
+            timestamp = purchase["timestamp"]
+            date = dateutil.parser.isoparse(timestamp).strftime("%Y-%m-%d")
+
+            for product in purchase["products"]:
+                product_name = product["name"]
+                unit_price = product["unitPrice"]
+                quantity = int(product["quantity"])
+
+                short_utskott = product_name.split("-")[0]
+                if not short_utskott in al.utskott_accounts:
+                    print(f"No utskott found for\n{product}\t{quantity=}\t{unit_price=}")
+                    continue
+
+                product_name = "".join(product_name.split("-")[1:])
+                utskott_account = al.utskott_accounts[short_utskott]
+                account = utskott_account["account"]
+                utskott_name = utskott_account["name"]
+
+                product_name_index = product_name + str(unit_price)
+
+                sale_key = "_".join([utskott_name, date, product_name_index])
+
+                if sale_key in sales:
+                    sales[sale_key]["quantity"] += quantity
+                else:
+                    sales[sale_key] = {
+                        "name": product_name,
+                        "quantity": quantity,
+                        "unit_price": unit_price,
+                        "account": account,
+                    }
+
+        def delimiter_splitter(key):
+            return key.split("_")
+
+        return unflatten(sales, splitter=delimiter_splitter)
 
     @staticmethod
     def get_data(start_date: datetime, end_date: datetime):
